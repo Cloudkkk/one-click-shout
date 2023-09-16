47010be19f6fb3a3ec3962aebf9d72e00cceb743
wr <Wrong2314@163.com>
2023-09-16 20:22:03 +0800

    feat: 添加监听控制

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,103 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
 import { listen } from '@tauri-apps/api/event';
+import { message, Form, Switch, Select, Input } from 'antd';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const listener = useRef(null);
+  const [form] = Form.useForm();
+  const [isAuto, setIsAuto] = useState(true);
+  const [selectedItem, setSelectedItem] = useState('');
+  const [textInput, setTextInput] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    let unlisten;
-    (async () => {
-      unlisten = await listen('f1_pressed', (event) => {
-        console.log('1111');
-      });
-    })();
-    return () => {
-      if (unlisten) {
-        unlisten();
+  const OPTIONS = [
+    { label: '按A', value: 'A' },
+    { label: '按S', value: 'S' },
+    { label: '按D', value: 'D' }
+  ]
+
+  /**
+   * @description 开启监听
+   */
+  const onOpenListen = async () => {
+    try {
+      await form.validateFields();
+      if (!listener.current) {
+        message.success('开始你的表演');
+        (async () => {
+          listener.current = await listen('f1_pressed', (event) => {
+            console.log('1111');
+          });
+        })();
       }
-    };
-  }, [count])
+    } catch (error) {
+      console.log(error);
+      setIsOpen(false);
+    }
+  }
+
+  /**
+   * @description 关闭监听
+   */
+  const onCloseListen = () => {
+    setIsOpen(false);
+    if (listener.current) {
+      message.info('演出结束了');
+      listener.current();
+      listener.current = null;
+    }
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Form
+          labelCol={{ span: 0 }}
+          wrapperCol={{ span: 24 }}
+          form={form}
+          style={{ minWidth: 200, maxWidth: 600 }}
+        >
+          <Form.Item>
+            <div>
+              <span className='mr10'>自己动</span>
+              <Switch
+                checked={isAuto}
+                onChange={(checked) => { setIsAuto(checked); onCloseListen(); }}
+              />
+              <span className='ml10'>全自动</span>
+            </div>
+          </Form.Item>
+          {
+            isAuto ? (
+              <Form.Item name='select' rules={[{ required: true, message: '请选择模式' }]}>
+                <Select
+                  value={selectedItem}
+                  options={OPTIONS}
+                  onChange={(value) => { setSelectedItem(value); onCloseListen(); }}
+                />
+              </Form.Item>
+            ) : (
+              <Form.Item name='input' rules={[{ required: true, message: '请输入内容' }]}>
+                <Input.TextArea
+                  value={textInput}
+                  rows={4}
+                  onInput={(e) => { setTextInput(e.target.value); onCloseListen(); }}
+                />
+              </Form.Item>
+            )
+          }
+          <Form.Item>
+            <Switch
+              checked={isOpen}
+              checkedChildren="开启"
+              unCheckedChildren="关闭"
+              onChange={(checked) => { setIsOpen(checked); checked ? onOpenListen() : onCloseListen(); }}
+            />
+          </Form.Item>
+        </Form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

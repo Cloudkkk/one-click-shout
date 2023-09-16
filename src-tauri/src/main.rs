b2bca7fed5fa59b5588bcef060ff2026c91499ef
Cloudkkk <445218339@qq.com>
2023-09-16 23:30:57 +0800

    feat:rust后端加入开关逻辑和自定义按键能力

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,25 +2,60 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // 引入device_query库，用于监控键盘事件
 use device_query::{DeviceQuery, DeviceState, Keycode};
+use enigo::{Enigo, KeyboardControllable, Key};
 use tauri::Manager;
 use std::sync::{Arc, Mutex};
+
+const USER_KEY_CHANNEL: &str = "user_key";
+const PRESS_CHANNEL: &str = "key_pressed";
+const SWITCH_CHANNEL: &str ="switch_channel"
+
 //主函数
 fn main() {
   let device_state = DeviceState::new();
+
   // 使用默认的tauri构建器
   tauri::Builder::default()
   .setup(move |app| {
         let window = app.get_window("main").unwrap();
         let window = Arc::new(Mutex::new(window));
         let window_clone = Arc::clone(&window);
-        std::thread::spawn(move || loop {
-            let keys = device_state.get_keys();
-            if keys.contains(&Keycode::A) {
-                let window = window_clone.lock().unwrap();
-                window.emit("f1_pressed", None::<()>).unwrap();
-            }
-            std::thread::sleep(std::time::Duration::from_millis(200));
+        // 克隆标志位的引用，以便在闭包中使用
+        let switch_flag_clone = Arc::clone(&switch_flag);
+
+        // 监听'switch_channel'事件
+        app.listen(SWITCH_CHANNEL, move |event| {
+            // 获取前端发送的开关状态
+            let switch_state = event.payload().unwrap_or(&"true".to_string()).parse::<bool>().unwrap_or(true);
+            // 更新标志位
+            *switch_flag_clone.lock().unwrap() = switch_state;
+            Ok(())
         });
+        // 在后端监听前端发送的事件
+        app.listen(USER_KEY_CHANNEL, move |event| {
+            let key_from_frontend = event.payload().unwrap_or("A").to_string();
+            let user_key = match key_from_frontend.as_str() {
+                "A" => Keycode::A,
+                "B" => Keycode::B,
+                // 你可以根据需要添加更多的匹配项
+                _ => Keycode::A, // 默认值
+            };
+            std::thread::spawn(move || loop {
+                // 检查标志位，如果为false则跳过本次循环
+                if !*switch_flag.lock().unwrap() {
+                    continue;
+                }
+                let mut enigo = Enigo::new();
+                let keys = device_state.get_keys();
+                if keys.contains(user_key) {
+                    let window = window_clone.lock().unwrap();
+                    enigo.key_down(Key::Shift);
+                    enigo.key_click(Key::Return);
+                    enigo.key_up(Key::Shift);
+                    window.emit(PRESS_CHANNEL, None::<()>).unwrap();
+                }
+                std::thread::sleep(std::time::Duration::from_millis(200));
+            });
         Ok(())
     })
     .run(tauri::generate_context!())
--- src/const/index.ts
+++ src/const/index.ts
@@ -1,2 +1,3 @@
-export const USERKEYCHANNEL = 'user_key'
-export const PRESSCHANNEL = 'key_pressed'
+export const USERKEY_CHANNEL = 'userkey_channel'
+export const KEY_PRESS_CHANNEL = 'key_press_channel'
+export const SWITCH_CHANNEL = 'switch_channel'

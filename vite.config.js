7c225a50edfeea8bd499f90f9c9781a31ad54348
yuanhaokai <yuanhaokai@meituan.com>
2023-09-16 10:14:23 +0800

    init tauri

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    port:3001
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+  }
+})
